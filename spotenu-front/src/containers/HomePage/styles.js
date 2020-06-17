ba2b8e9d2235d3993e40a90655708fead96b3021
Galanzito <gui_galan@hotmail.com>
2020-06-17 15:46:16 -0300

    Resolvendo Bugs

---
+++ spotenu-front/src/containers/HomePage/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import Card from '@material-ui/core/Card'
+
+export const CardWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    max-width: 800px;
+    gap:50px;
+
+    div{
+        width: 25vw;
+        margin-top: 10px;
+`;
+
+export const CardContainer = styled(Card)`
+    img{
+        width:100%;
+        height: 25vh;
+    }
+
+`
