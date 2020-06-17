07cc817ac122feea91a2cb162a068bd001683a10
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-06-17 02:59:43 -0300

    Pequenos ajustes

--- spotenu-front/src/containers/Router/index.js
+++ spotenu-front/src/containers/Router/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
-import HomePage from '../HomePage/HomePage';
+import LoginPage from '../LoginPage/LoginPage';
 import SignUpPage from '../SignUpPage/SignUpPage';
 
 export const routes = {
-    home: "/",
+    login: "/",
     signup: "/signup"
-    
+
 }
 
 function Router(props) {
-    return(
+    return (
         <ConnectedRouter history={props.history}>
             <Switch>
-                <Route exact path={routes.home} component={HomePage} />
+                <Route exact path={routes.login} component={LoginPage} />
                 <Route exact path={routes.signup} component={SignUpPage} />
             </Switch>
         </ConnectedRouter>
